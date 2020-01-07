7bb14dd898e99b7df69c63a09bbcd00f882ca5bc
Tom Brus <tombrus@gmail.com>
2020-01-07 23:56:59 +0100

    adjust build

--- project.sh
+++ project.sh
@@ -27,8 +27,8 @@ artifacts=(
   "org.modelingvalue   dclareForJava           0.0.3       jar jds"
 )
 dependencies=(
-  "org.modelingvalue   dclare                  0.0.6       jar jds-"
-  "org.modelingvalue   immutable-collections   1.0.15      jar jds-"
+  "org.modelingvalue   dclare                  0.0.7       jar jds-"
+  "org.modelingvalue   immutable-collections   1.0.16      jar jds-"
 
   "junit               junit                   4.12        jar jdst"
   "org.hamcrest        hamcrest-core           1.3         jar jdst"
