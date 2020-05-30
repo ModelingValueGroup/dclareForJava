7db56bda3db05f6a49c0d7c7d1a42f80fb47d8c8
Tom Brus <tombrus@gmail.com>
2020-05-31 00:17:03 +0200

    build details

--- project.sh
+++ project.sh
@@ -13,13 +13,13 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="1.2.2"
+version="1.2.3"
 artifacts=(
   "org.modelingvalue   dclareForJava           $version    jar jds"
 )
 dependencies=(
-  "org.modelingvalue   dclare                  1.2.2       jar jds-"
-  "org.modelingvalue   immutable-collections   1.2.2       jar jds-"
+  "org.modelingvalue   dclare                  1.2.3       jar jds-"
+  "org.modelingvalue   immutable-collections   1.2.3       jar jds-"
 
   "junit               junit                   4.12        jar jdst"
   "org.hamcrest        hamcrest-core           1.3         jar jdst"
