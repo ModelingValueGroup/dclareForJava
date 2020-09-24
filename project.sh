6d9d24544935a6aac11d8526bf0fd08b767f6563
rkrijgsheld <rkrijgsheld@gmail.com>
2020-09-24 12:59:48 +0000

    DCL-165 update versions

--- project.sh
+++ project.sh
@@ -13,13 +13,13 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="1.3.0"
+version="1.4.0"
 artifacts=(
   "org.modelingvalue   dclareForJava           $version    jar jds"
 )
 dependencies=(
-  "org.modelingvalue   dclare                  1.3.0       jar jds-"
-  "org.modelingvalue   immutable-collections   1.3.0       jar jds-"
+  "org.modelingvalue   dclare                  1.4.0       jar jds-"
+  "org.modelingvalue   immutable-collections   1.4.0       jar jds-"
 
   "org.junit.jupiter   junit-jupiter-api       5.6.2       jar jdst"
   "org.junit.jupiter   junit-jupiter-engine    5.6.2       jar jdst"
