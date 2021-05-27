9c0ea828b3f3afaacd655a86c0a81685e4c99e94
Tom Brus <tombrus@gmail.com>
2021-05-27 23:40:40 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,9 @@ defaultTasks("mvgCorrector", "test", "publish", "mvgTagger")
 plugins {
     `java-library`
     `maven-publish`
-    id("org.modelingvalue.gradle.mvgplugin") version "0.4.21"
+    id("org.modelingvalue.gradle.mvgplugin") version "0.4.29"
 }
 dependencies {
-    
-    
-    //testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     implementation("org.modelingvalue:dclare:1.5.0-BRANCHED")
     implementation("org.modelingvalue:immutable-collections:1.5.0-BRANCHED")
     implementation("com.intellij:forms_rt:7.0.3")
