af04335d77d993d32773195ca94cfd4ddd5a330f
Tom Brus <tombrus@gmail.com>
2021-08-26 12:49:19 +0200

    prepare for release using gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ defaultTasks("mvgCorrector", "test", "publish", "mvgTagger")
 plugins {
     `java-library`
     `maven-publish`
-    id("org.modelingvalue.gradle.mvgplugin") version "0.4.35"
+    id("org.modelingvalue.gradle.mvgplugin") version "0.4.38"
 }
 dependencies {
     implementation("org.modelingvalue:dclare:1.5.0-BRANCHED")
