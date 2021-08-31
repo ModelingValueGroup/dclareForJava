23fc12fa2520e20670d9aa31a9981fe4e048613a
WimBast <wim.bast@gmail.com>
2021-08-31 14:19:22 +0200

    dCheckConsistency override

--- src/main/java/org/modelingvalue/jdclare/DObject.java
+++ src/main/java/org/modelingvalue/jdclare/DObject.java
@@ -126,6 +126,11 @@ public interface DObject extends DStruct, Mutable {
     }
 
     @Override
+    default boolean dCheckConsistency() {
+        return Mutable.super.dCheckConsistency(); // do not remove this! it seems unneccesarry but it is not; this has to do with how Proxy handles calls.
+    }
+
+    @Override
     default MutableTransaction openTransaction(MutableTransaction parent) {
         return Mutable.super.openTransaction(parent);
     }
