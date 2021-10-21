1251d9a4483344cb36153792e72473988ee88177
Tom Brus <tombrus@gmail.com>
2021-10-22 01:40:44 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ defaultTasks("mvgCorrector", "test", "publish", "mvgTagger")
 plugins {
     `java-library`
     `maven-publish`
-    id("org.modelingvalue.gradle.mvgplugin") version "1.0.1"
+    id("org.modelingvalue.gradle.mvgplugin") version "1.0.6"
 }
 dependencies {
     implementation("org.modelingvalue:dclare:1.6.1-BRANCHED")
