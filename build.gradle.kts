26621b98b3b042dc0e358787667787d0a42f0a0a
Tom Brus <tombrus@gmail.com>
2026-02-26 22:31:46 +0100

    upgrade to java21 and gradle 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ defaultTasks("mvgCorrector", "test", "publish", "mvgTagger")
 plugins {
     `java-library`
     `maven-publish`
-    id("org.modelingvalue.gradle.mvgplugin") version "1.1.3"
+    id("org.modelingvalue.gradle.mvgplugin") version "2.3.15"
 }
 dependencies {
     implementation("org.modelingvalue:dclare:4.1.0-BRANCHED")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://modelingvaluegroup.github.io/gradlePlugins/") }
+        gradlePluginPortal()
+    }
+}
+
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //  (C) Copyright 2018-2026 Modeling Value Group B.V. (http://modelingvalue.org)                                         ~
 //                                                                                                                       ~
@@ -21,7 +28,3 @@
 rootProject.name = "dclare-for-java"
 
 include("ext","examples")
-
-plugins {
-    id("com.gradle.enterprise") version ("3.5")
-}
